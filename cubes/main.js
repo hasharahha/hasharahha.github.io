06cc0e5d1e7694f701238c1f586321d9a58e6f41
Jacob Bondley <hasharahha@gmail.com>
2014-12-09 23:27:11 -0500

    Update main.js

--- cubes/main.js
+++ cubes/main.js
@@ -1,5 +1,12 @@
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
+var speed = 3;
+var player ={
+  x: 10,
+  y: 10,
+  width: 20,
+  height: 20
+}
 
 var keys =[];
 
@@ -9,3 +16,21 @@ window.addEventListener("keydown", function(e){
 window.addEventListener("keyup", function(e){
   keys[e.keyCode] = false;
 })
+
+
+function game(){
+  update();
+  render();
+}
+function update(){
+if(keys[38]){
+  player.y -= speed;
+}  
+}
+function render(){
+  ctx.fillRect(player.x, player.y, player.width, player.height);
+}
+
+setInterval(function(){
+  game();
+}, 1000/30)

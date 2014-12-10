2f5c503861933a8ebbb26ffeb654a27585f7007e
Jacob Bondley <hasharahha@gmail.com>
2014-12-10 08:08:03 -0500

    Update main.js

--- cubes/main.js
+++ cubes/main.js
@@ -48,7 +48,7 @@ function update(){
     process();
   }
   if(collision(enemy, player)){
-    Respawn();
+    setTimeout("Respawn()", 2000);
   }
   if(score.health < 1){
     document.getElementById("Game").style.display='none';

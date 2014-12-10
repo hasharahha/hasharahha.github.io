96edf454f8c28ee97bea29b5c58407a93b88a931
Jacob Bondley <hasharahha@gmail.com>
2014-12-10 11:57:35 -0500

    Update main.js

--- cubes/main.js
+++ cubes/main.js
@@ -117,9 +117,9 @@ function Respawn(){
 
 function collision(first, second){
   return !(first.x > second.x + second.width||
-  first.x + first.height < second.width||
+  first.x + first.height < second.x||
   first.y > second.y + second.height||
-  first.y + first.width < second.height)
+  first.y + first.width < second.y)
   
   
 }

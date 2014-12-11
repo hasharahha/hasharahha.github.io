1a166c373283e0245f73186efb2d2114e543dda4
Jacob Bondley <hasharahha@gmail.com>
2014-12-11 12:29:29 -0500

    Update main.js

--- Adventurer/main.js
+++ Adventurer/main.js
@@ -1,3 +1,19 @@
 //Thanks for using my games, Feel free to use my code
 //By jacob Bondley
 var canvas = document.getElementById("game");
+var ctx = canvas.getContext("2d");
+var static;
+var player_1 ={
+  x: 20,
+  y: 20,
+  height: 10,
+  width: 10,
+  speed: 3,
+  score: 0,
+  fuel: 6000,
+  color:"yellow"
+}
+/*
+Controls:
+
+*/

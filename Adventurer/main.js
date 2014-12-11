4be1894a9a85e8d5237ca879cceb4ba0defaac0f
Jacob Bondley <hasharahha@gmail.com>
2014-12-11 13:06:35 -0500

    Update main.js

--- Adventurer/main.js
+++ Adventurer/main.js
@@ -3,7 +3,8 @@
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 var static;
-var player_1 ={
+var keys =[];
+var player1 ={
   x: 20,
   y: 20,
   height: 10,
@@ -12,7 +13,25 @@ var player_1 ={
   score: 0,
   fuel: 6000,
   color:"yellow"
-}
+};
+var player2 ={
+  x: 20,
+  y: 300,
+  height: 10,
+  width: 10,
+  speed: 3,
+  score: 0,
+  fuel: 6000,
+  color:"blue"
+};
+
+window.addEventListener("keydown", function(e){
+  keys[e.keyCode] = true;
+});
+window.addEventListener("keyup", function(e){
+  keys[e.keyCode] = false;
+});
+
 /*
 Controls:
 Player_1: 
@@ -27,3 +46,31 @@ Uarrow UP [38]
 C CHARGE [67]
 
 */
+
+
+function update(){
+  if(keys[65]){
+    player1.x -= player1.speed;
+  }
+  if(keys[68]){
+    player1.x += player1.speed;
+  }
+}
+function process(){
+  
+}
+function render(){
+  
+}
+
+function gameloop(){
+  update();
+  process();
+  render();
+}
+
+
+setInterval(function(){
+  gameloop();
+}, 1000/30)
+

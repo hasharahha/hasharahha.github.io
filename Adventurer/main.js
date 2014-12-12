bea2114b7fbdf0c8f1d7128f95dad368b5d48e51
Jacob Bondley <hasharahha@gmail.com>
2014-12-11 23:07:21 -0500

    Update main.js

--- Adventurer/main.js
+++ Adventurer/main.js
@@ -85,6 +85,15 @@ function render(){
   //Scores
 }
 
+function collision(first, second){
+  return !(first.x > second.x + second.width||
+  first.x + first.height < second.x||
+  first.y > second.y + second.height||
+  first.y + first.width < second.y)
+  
+  
+}
+
 function gameloop(){
   update();
   process();
@@ -95,3 +104,4 @@ function gameloop(){
 setInterval(function(){
   gameloop();
 }, 1000/30)
+

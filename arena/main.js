95d275ee0d9fc512e2c5b16b6666818e128dadad
Jacob Bondley <hasharahha@gmail.com>
2014-12-13 14:22:30 -0500

    Create main.js

---
+++ arena/main.js
@@ -0,0 +1,15 @@
+var canvas = document.getElementById("game");
+var ctx = canvas.getContext("2d");
+var static;
+//game class
+var game ={
+height: window.innerHeight,
+width: window.innerWidth
+fps: 1000/30,
+speed: 1,
+bg:gray
+}
+
+document.getElementById("game").setAttribute("style", "background-color: "+game.bg);
+document.getElementById("game").setAttribute("height", game.height);
+document.getElementById("game").setAttribute("width", game.width);

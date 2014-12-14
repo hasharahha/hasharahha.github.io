087079ee1323e19489091f7c6b8eecd94e2335db
Jacob Bondley <hasharahha@gmail.com>
2014-12-13 21:52:30 -0500

    Update main.js

--- arena/main.js
+++ arena/main.js
@@ -1,3 +1,16 @@
 //And the Game begins
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
+var static;
+var keys =[];
+
+//player class
+var player ={
+  height: 32,
+  width: 16,
+  src: "https://dl.dropboxusercontent.com/s/006jpxndv604hgk/player.png",
+  eastX: -34,
+  eastY: 0,
+  westX: -51,
+  westY: 0
+}

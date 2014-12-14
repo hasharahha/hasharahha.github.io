15f820700ea54d0bacdf979eda9ac7318c935696
Jacob Bondley <hasharahha@gmail.com>
2014-12-13 22:14:26 -0500

    Update main.js

--- arena/main.js
+++ arena/main.js
@@ -33,7 +33,7 @@ var playerimg = new Image();
 playerimg.src ="https://dl.dropboxusercontent.com/s/006jpxndv604hgk/player.png";
 playerimg.onload = function(){
   player.loaded =true;
-ctx.drawImage(playerimg, player.x, player.y, player.width, player.height, playerDirectionX, playerDirectionY, player.width, player.height);
+ctx.drawImage(playerimg, player.x, player.y, player.width, player.height);
 };
 //Detecters
 window.addEventListener("keydown", function(e){

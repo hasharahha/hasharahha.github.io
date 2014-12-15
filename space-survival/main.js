47c5c5122d757f10d184d6bda76f665c5706eb09
Jacob Bondley <hasharahha@gmail.com>
2014-12-15 07:55:42 -0500

    Update main.js

--- space-survival/main.js
+++ space-survival/main.js
@@ -4,7 +4,9 @@ var ctx = canvas.getContext("2d");
 var game ={
   height: 500,
   width: 600,
-  fps: 1000/30
+  fps: 1000/30,
+  color:"gray"
 }
 canvas.height = game.height;
 canvas.width = game.width;
+canvas.style.backgroundColor = game.color;

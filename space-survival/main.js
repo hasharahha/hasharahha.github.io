685f7d17677885eca05fc60772fa3402fce1b675
Jacob Bondley <hasharahha@gmail.com>
2014-12-15 07:54:25 -0500

    Update main.js

--- space-survival/main.js
+++ space-survival/main.js
@@ -1,2 +1,10 @@
 //All game logic and rendering
 var canvas = document.getElementById("game");
+var ctx = canvas.getContext("2d");
+var game ={
+  height: 500,
+  width: 600,
+  fps: 1000/30
+}
+canvas.height = game.height;
+canvas.width = game.width;

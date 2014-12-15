36700fffed3f23d10d83e111817315cb83c6593b
Jacob Bondley <hasharahha@gmail.com>
2014-12-15 17:13:34 -0500

    Update main.js

--- space-survival/main.js
+++ space-survival/main.js
@@ -1,12 +1,51 @@
 //All game logic and rendering
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
+var static;
+var keys =[];
 var game ={
   height: 600,
   width: 800,
   fps: 1000/30,
   color:"gray"
 }
+var player1 ={
+  x: 10,
+  y: 10,
+  pos: 0,
+  fuel: 1000,
+  score: 0
+};
 canvas.height = game.height;
 canvas.width = game.width;
-canvas.style.backgroundColor = game.color;
+
+window.addEventListener("keydown", function(e){
+  keys[e.keyCode] = true;
+}, false);
+window.addEventListener("keyup", function(e){
+  keys[e.keyCode] = false;
+}, false);
+
+
+
+
+
+function update(){
+  
+}
+function process(){
+  
+}
+function render(){
+  
+}
+
+
+function gameloop(){
+  update();
+  process();
+  render();
+  window.requestAnimationFrame(function(){
+    gameloop();
+  }, game.fps);
+}

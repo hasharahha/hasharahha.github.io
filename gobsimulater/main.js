2f360df0dd374af0ec7101dac59133f52fa58e74
Jacob Bondley <hasharahha@gmail.com>
2014-12-27 16:59:34 -0500

    Update main.js

--- gobsimulater/main.js
+++ gobsimulater/main.js
@@ -2,19 +2,13 @@ var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 var static;
 var skin = new Image();
-skin.src ="https://dl.dropboxusercontent.com/s/o0e18h3fc5d2jsd/character_silver.png"
+skin.src ="https://dl.dropboxusercontent.com/s/o0e18h3fc5d2jsd/character_silver.png";
 var player ={
   x: 10,
   y: 10,
   height: 42,
   width: 42,
-  stage: "down",
-  update: function(){
-    ctx.drawImage(skin, 0, 0, this.height, this.width, this.x, this.y, this.height, this.width);
-  }
-}
+  stage: "idol"
+};
 
 alert("Hello");
-setInterval(function(){
-  player.update();
-}, 1000);

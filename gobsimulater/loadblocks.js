005cb8a7a46ddf7ce9dfdc23aa57a03cc3e3ccd6
Jacob Bondley <hasharahha@gmail.com>
2014-12-29 13:33:11 -0500

    Update loadblocks.js

--- gobsimulater/loadblocks.js
+++ gobsimulater/loadblocks.js
@@ -6,32 +6,7 @@ var blockpath ={};
 var blocktile = new Image();
 blocktile.src ="https://dl.dropboxusercontent.com/s/3qb4u8ws3ehqops/block_tiles.jpg"
 blocktile.onload = function(){
-  drawblock();
+setb.drawImage(blocktile, 160, 0, 32, 32, 64, 32, 32, 32);
 };
-/*
-function chooseblock(){
-  if(rnd < 100){
-    var block ="BRICK";
-    blockpath.x = randomWidth;
-    blockpath.y = randomHeight;
-    blockpath.spriteX =0;
-    blockpath.spriteY =96;
-    blockpath.height =32;
-    blockpath.width =32;
-    drawblock();
-  }
-}
-*/
 
-function drawblock(){
-  setb.drawImage(blocktile, blockpath.spriteX, blockpath.spriteY, 32, 32, blockpath.x, blockpath.y, 32, 32);
-}
-function rndtick(){
-  rnd = Math.floor(Math.random() *460);
-  var randomWidth = Math.floor(Math.random()*860);
-  var randomHeight = Math.floor(Math.random()*460);
-}
 
-setInterval(function(){
-  rndtick();
-}, 500);

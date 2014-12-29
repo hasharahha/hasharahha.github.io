766012366d0d9962762f04700ce2ccc48b42ab64
Jacob Bondley <hasharahha@gmail.com>
2014-12-29 13:37:33 -0500

    Update loadblocks.js

--- gobsimulater/loadblocks.js
+++ gobsimulater/loadblocks.js
@@ -7,7 +7,7 @@ var blocktile = new Image();
 blocktile.src ="https://dl.dropboxusercontent.com/s/3qb4u8ws3ehqops/block_tiles.jpg"
 blocktile.onload = function(){
 setb.drawImage(blocktile, 160, 0, 32, 32, 64, 32, 32, 32);
-setb.drawImage(blocktile, 192, 0, 32, 32, 64, 544, 32, 32);
+setb.drawImage(blocktile, 192, 0, 32, 32, 64, 256, 32, 32);
 };
 
 

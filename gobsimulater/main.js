034b45a2bad3017fa340a8ffbcf80531b0bf4882
Jacob Bondley <hasharahha@gmail.com>
2014-12-29 20:19:00 -0500

    Update main.js

--- gobsimulater/main.js
+++ gobsimulater/main.js
@@ -104,6 +104,7 @@ function render(){
   ctx.fillStyle ="white";
   ctx.fillText("Player X: "+getPlayerX(), 20, 430);
   ctx.fillText("Player Y: "+getPlayerY(), 140, 430);
+  ctx.fillText("
 }
 
 function getPlayerX(){
@@ -117,6 +118,24 @@ function getPlayerY(){
   static = Math.abs(static);
   return static;
 }
+var fps = {
+	startTime : 0,
+	frameNumber : 0,
+	getFPS : function(){
+		this.frameNumber++;
+		var d = new Date().getTime(),
+			currentTime = ( d - this.startTime ) / 1000,
+			result = Math.floor( ( this.frameNumber / currentTime ) );
+
+		if( currentTime > 1 ){
+			this.startTime = new Date().getTime();
+			this.frameNumber = 0;
+		}
+		return result;
+
+	}	
+};
+
 function gameloop(){
   update();
   Animate();

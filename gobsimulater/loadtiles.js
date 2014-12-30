275575040b7e837a6fc8faa0fee4588c0ee9b2fb
Jacob Bondley <hasharahha@gmail.com>
2014-12-30 00:02:14 -0500

    Update loadtiles.js

--- gobsimulater/loadtiles.js
+++ gobsimulater/loadtiles.js
@@ -79,9 +79,9 @@ tileset.onload = function(){
   
   
   
-  start();
   
   set.fillRect(0, 0, 1000, 500);
+  start();
   set.drawImage(tileset, tiles.o1[1], tiles.o1[2], tiles.o1[3], tiles.o1[4], tiles.o1[5], tiles.o1[6], tiles.o1[7], tiles.o1[8]);
   set.drawImage(tileset, tiles.o2[1], tiles.o2[2], tiles.o2[3], tiles.o2[4], tiles.o2[5], tiles.o2[6], tiles.o2[7], tiles.o2[8]);  
   set.drawImage(tileset, tiles.o3[1], tiles.o3[2], tiles.o3[3], tiles.o3[4], tiles.o3[5], tiles.o3[6], tiles.o3[7], tiles.o3[8]);  

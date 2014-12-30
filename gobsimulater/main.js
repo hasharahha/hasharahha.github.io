fc6aa69a5138f5ae1c229adad574b09ef781c473
Jacob Bondley <hasharahha@gmail.com>
2014-12-29 19:39:26 -0500

    Update main.js

--- gobsimulater/main.js
+++ gobsimulater/main.js
@@ -5,7 +5,6 @@ var keys =[];
 var skin = new Image();
 skin.src ="https://dl.dropboxusercontent.com/s/o0e18h3fc5d2jsd/character_silver.png";
 
-var url = skin.src.toDataURL();
 var player ={
   x: 10,
   y: 10,

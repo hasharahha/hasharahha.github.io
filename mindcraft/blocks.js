05a9f0f2ac0921b36ebac030645d9576a0ec847c
Jacob Bondley <hasharahha@gmail.com>
2014-12-31 14:40:54 -0500

    Update blocks.js

--- mindcraft/blocks.js
+++ mindcraft/blocks.js
@@ -1,1 +1,4 @@
-//
+var c = document.getElementById("terrain");
+var context = c.getContext("2d");
+var block = new Image();
+block.src ="https://dl.dropboxusercontent.com/s/8l8t9xx1qybg35w/blocks.png";

b6c2b811adf921079bcda50e6980caefcb0bec55
Jacob Bondley <hasharahha@gmail.com>
2014-12-30 23:30:08 -0500

    Update player.js

--- mindcraft/player.js
+++ mindcraft/player.js
@@ -1,1 +1,25 @@
-//Player config
+var canvas = document.getElementById("game");
+var ctx = canvas.getContext("2d");
+var static;
+var playerimg = new Image();
+playerimg.src ="https://dl.dropboxusercontent.com/s/1jp0m4zu92697hw/player.png";
+var game ={
+  height: 800,
+  width: 1200,
+  ping: 1000/30
+};
+
+
+
+
+
+function gameloop(){
+  update();
+  process();
+  render();
+  
+  
+  window.requestAnimationFrame(function(){
+    gameloop();
+  }, game.ping)
+}

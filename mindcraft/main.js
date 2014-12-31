a13ae2557c6efaeaf8283635912e28c034f53da2
Jacob Bondley <hasharahha@gmail.com>
2014-12-31 01:48:05 -0500

    Update main.js

--- mindcraft/main.js
+++ mindcraft/main.js
@@ -2,11 +2,13 @@ var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 var static;
 var playerimg = new Image();
+var blocks = new Image();
+blocks.src ="https://dl.dropboxusercontent.com/s/8l8t9xx1qybg35w/blocks.png"
 playerimg.src ="https://dl.dropboxusercontent.com/s/1jp0m4zu92697hw/player.png";
 var keys =[];
 var game ={
   height: 600,
-  width: 1300,
+  width: 1200,
   ping: 1000/30
 };
 
@@ -31,9 +33,9 @@ function process(){
 function render(){
 canvas.height = game.height;
 canvas.width = game.width;
-ctx.clearRect(0, 0, game.width, game.height);
+ctx.clearRect(player.x, player.y, player.width, player.height);
 ctx.drawImage(playerimg, player.spriteX, player.spriteY, player.width, player.height, player.x, player.y, player.width, player.height);
-
+loadTerrain();
 }
 
 function gameloop(){
@@ -43,4 +45,16 @@ render();
 window.requestAnimationFrame(function(){
  gameloop(); 
 }, game.ping);
+}
+
+
+function loadTerrain(){
+  var a;
+  var ax =0;
+  var ay =0;
+  for(a=0; a < 32; a++){
+    ax +=32;
+    ay = game.height-128;
+    ctx.drawImage(blocks, 0, 0, 32, 32, ax, ay, 32, 32);
+  }
 }

c2e4092f9d0eda7cc91f15e1c8e8e3796e898274
Jacob Bondley <hasharahha@gmail.com>
2014-12-31 15:51:39 -0500

    Update blocks.js

--- mindcraft/blocks.js
+++ mindcraft/blocks.js
@@ -23,6 +23,7 @@ c.addEventListener("touchstart", function(e){
   click.y = e.touches[0].pageY 
   if(click.y < 400){
     click.y = click.y -=20;
+    click.x = click.x -=10;
     context.drawImage(blocks, 448, 0, 32, 32, click.x, click.y, 32, 32);
   }
   

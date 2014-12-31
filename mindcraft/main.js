cb606d4ddcc5728321147b842dd5df8925406661
Jacob Bondley <hasharahha@gmail.com>
2014-12-31 01:27:43 -0500

    Update main.js

--- mindcraft/main.js
+++ mindcraft/main.js
@@ -1,1 +1,29 @@
-//main script
+var canvas = document.getElementById("game");
+var ctx = canvas.getContext("2d");
+var static;
+var game ={
+  height: 600,
+  width: 1300,
+  ping: 1000/30,
+  update: function(){
+    
+  },
+  process: function(){
+    
+  },
+  render: function(){
+    canvas.height = this.height;
+    canvas.width = this.width;
+  }
+}
+
+
+function gameloop(){
+game.update();
+game.process();
+game.render();
+player.update();
+window.requestAnimationFrame(function(){
+ gameloop(); 
+}, game.ping);
+}

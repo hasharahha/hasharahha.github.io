12e720a49028b2c0c3a0a3fe9cbb5fa90b25b326
Jacob Bondley <hasharahha@gmail.com>
2015-01-03 00:05:04 -0500

    Update process.js

--- skins/process.js
+++ skins/process.js
@@ -1,1 +1,28 @@
-//
+var canvas = document.getElementById("skin");
+var ctx = canvas.getContext("2d");
+var static;
+var skins = new Image();
+skins.src ="https://dl.dropboxusercontent.com/s/wm2cn84lml3bf3q/all_skins.png";
+
+var selected_skin ={
+  height: 42,
+  width: 42,
+  x: 0,
+  y: 0,
+  spriteX: 0,
+  spriteY: 0
+};
+
+function process(){
+  
+}
+function render(){
+  
+}
+function loop(){
+  process();
+  render();
+  window.requestAnimationFrame(function(){
+    loop();
+  }, 500);
+}

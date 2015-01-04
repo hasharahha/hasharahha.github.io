d08ca6f2f1a214f9f4188348ee503157cbfa4232
Jacob Bondley <hasharahha@gmail.com>
2015-01-04 01:08:35 -0500

    Update blocks.js

--- mindcraft/blocks.js
+++ mindcraft/blocks.js
@@ -2,6 +2,7 @@ var c = document.getElementById("terrain");
 var context = c.getContext("2d");
 var block = new Image();
 block.src ="https://dl.dropboxusercontent.com/s/8l8t9xx1qybg35w/blocks.png";
+alert("Genrating terrain..")
 
 block.onload = function(){
   updateloop();

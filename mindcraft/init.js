516b2380c7712e7e1eeb0ad4db35e3b5102e3a50
Jacob Bondley <hasharahha@gmail.com>
2015-01-04 04:55:14 -0500

    Update init.js

--- mindcraft/init.js
+++ mindcraft/init.js
@@ -1,1 +1,23 @@
-alert("hello");
+var screen = document.getElementById("indicators");
+var icon = screen.getContext("2d");
+//this is where all button's will go
+var click ={
+  height: 32,
+  width: 32,
+  x: 0,
+  y: 0
+}
+screen.addEventListener("touchstart", function(e){
+  click.x = touches[0].pageX;
+  click.y = touches[0].pageY;
+}, false);
+
+screen.addEventListener("touchend", function(e){
+  click.x =0;
+  click.y =0;
+}, false);
+
+function process(){
+  
+}
+

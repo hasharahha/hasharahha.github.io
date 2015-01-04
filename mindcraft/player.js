d64814dfe8b224fa1aa30b9c79ab59cfb61ae36b
Jacob Bondley <hasharahha@gmail.com>
2015-01-04 03:26:16 -0500

    Update player.js

--- mindcraft/player.js
+++ mindcraft/player.js
@@ -5,7 +5,7 @@ display.width =1024;
 var static;
 var playerimg = new Image();
 playerimg.src ="https://dl.dropboxusercontent.com/s/1jp0m4zu92697hw/player.png";
-
+alert("Player loaded");
 var player ={
   width: 32,
   height: 64,
@@ -15,5 +15,19 @@ var player ={
   inventory: [0, 0, 0, 0, 0, 0, 0, 0, 0],
   stage: "idol",
   spriteX: 0,
-  spriteY: 0
+  spriteY: 0,
+  loaded: true,
+  weight: 2,
+  speed: 3
 };
+
+function playerRender(){
+  players.drawImage(playerimg, player.spriteX, player.spriteY, player.width, player.height, player.x, player.y, 32, 64);
+}
+
+funtion playerloop(){
+  playerRender();
+  window.requestAnimationFrame(function(){
+    playerloop();
+  }, 1000/30);
+}

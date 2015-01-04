a97f0db446b68f05386bd8c5bb72ba3f212bb2fa
Jacob Bondley <hasharahha@gmail.com>
2015-01-04 14:50:51 -0500

    Update player.js

--- mindcraft/player.js
+++ mindcraft/player.js
@@ -16,7 +16,7 @@ var player={
   y: 0,
   stage: "idol",
   update: function(){
-    context.clearRect(this.x, this.y, this.width, this.height);
+    context.clearRect(this.x, this.y -1, this.width, this.height);
     context.drawImage(playersprite, this.spriteX, this.spriteY, this.width, this.height, this.x, this.y, this.width, this.height);
   }
 };

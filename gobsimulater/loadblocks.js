0a44866f9403193436d8cde93fe755351cc4ca7e
Jacob Bondley <hasharahha@gmail.com>
2015-01-03 21:54:27 -0500

    Update loadblocks.js

--- gobsimulater/loadblocks.js
+++ gobsimulater/loadblocks.js
@@ -2,14 +2,24 @@
 var blockCanvas = document.getElementById("blocktiles");
 var setb = blockCanvas.getContext("2d");
 var rnd;
-var blockpath ={};
 var blocktile = new Image();
 blocktile.src ="https://dl.dropboxusercontent.com/s/3qb4u8ws3ehqops/block_tiles.jpg"
 blocktile.onload = function(){
-setb.drawImage(blocktile, 160, 0, 32, 32, 64, 32, 32, 32);
-setb.drawImage(blocktile, 192, 0, 32, 32, 256, 0, 32, 32);
-setb.drawImage(blocktile, 128, 0, 32, 32, 32, 288, 32, 32);
-setb.drawImage(blocktile, 160, 0, 32, 416, 32, 32, 32, 32);
-};
+loadloop();
+}
+var blockpath =[
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
 
+function renderblcoks(){
+  
+}
 
+function loadloop(){
+  renderblocks();
+  window.requestAnimationFrame(function(){
+    loadloop();
+  }, 100);
+}

a3acf01e1889998b7fa80afd94242bd31d6bb42c
Jacob Bondley <hasharahha@gmail.com>
2015-01-05 16:25:06 -0500

    Update allow.js

--- mindcraft/Auth/allow.js
+++ mindcraft/Auth/allow.js
@@ -1,4 +1,10 @@
 function Auth(){
 var user = document.getElementById("username").value;
 var pass = document.getElementById("password").value;
+
+if(user =="HASHARAHHA14"&&pass =="JAB"){
+  localStorage.setItem("user", "true");
+  window.location ='http://hasharahha.github.io/mindcraft/dev.html';
+}
+
 }

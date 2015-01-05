81790093627249d94f33784d8435ebb69604445f
Jacob Bondley <hasharahha@gmail.com>
2015-01-05 16:22:22 -0500

    Create check.js

---
+++ mindcraft/check.js
@@ -0,0 +1,9 @@
+function Authenticate(){
+var user = document.getElementById("username").value;
+var pass = document.getElementById("password").value;
+
+if(user =="HASHARAHHA14"&&pass=="JAB"){
+localStorage.setItem("user", "true");
+}
+
+}

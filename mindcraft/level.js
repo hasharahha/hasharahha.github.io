abeb0f6e81bde8b912e67e5e31837514eb31f3d9
Jacob Bondley <hasharahha@gmail.com>
2015-01-05 20:16:56 -0500

    Update level.js

--- mindcraft/level.js
+++ mindcraft/level.js
@@ -85,11 +85,11 @@ function gravity(){
     if(terrain[Math.round(player.y/32) +1][Math.round(player.x/32)] ==0){
       player.y += player.weight;
     }
-    if(terrain[Math.round(player.y/32)][Math.round(player.x/32)] >=1){
-      player.x +=0;
+    if(terrain[Math.round(player.y/32)][Math.round(player.x/32)-1] >=15){
+      player.x +=1;
     }
-    if(terrain[Math.round(player.y/32)][Math.round(player.x/32)] >=1){
-      player.x -=0;
+    if(terrain[Math.round(player.y/32)][Math.round(player.x/32)-1] >=15){
+      player.x -=1;
     }
     
   }catch(e){

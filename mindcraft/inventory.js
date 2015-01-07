864a2854a3976757c1c3a144ded81b59911497fd
Jacob Bondley <hasharahha@gmail.com>
2015-01-07 18:07:03 -0500

    Update inventory.js

--- mindcraft/inventory.js
+++ mindcraft/inventory.js
@@ -3,9 +3,21 @@
 Notes:
 Uses icon, so display is over writting terrain
 */
+check();
 function check(){
-
-
-
-
+try{
+if(player.inventory[0][0] ==1){
+  icon.drawImage(blocks, 0, 0, 32, 32, 73, 73, 30, 30);
+  icon.fillText(player.inventory[0][1], 72, 90);
+}
+if(player.inventory[0][0] ==2){
+  icon.drawImage(blocks, 32, 0, 32, 32, 105, 73, 30, 30);
+  icon.fillText(player.inventory[0][1], 104, 73, 30, 30);
+}
+}catch(e){
+  error =e;
+}
+setTimeout(function(){
+  check();
+}, 1000/30);
 }

82ed859b36f4c804886e8cfa33d2d1bf42cc58f2
Jacob Bondley <hasharahha@gmail.com>
2015-01-07 17:57:04 -0500

    Update player.js

--- mindcraft/player.js
+++ mindcraft/player.js
@@ -4,8 +4,6 @@ c.height =512;
 var context = c.getContext("2d");
 var playersprite = new Image();
 playersprite.src ="https://dl.dropboxusercontent.com/s/1jp0m4zu92697hw/player.png";
-var zombieSprite = new Image();
-zombieSprite.src ="https://dl.dropboxusercontent.com/s/3cz7l0dllf7mibd/zombie.png";
 var player={
   name:"Steve",
   inventory: [[1, 1],[0, 0],[0, 0],[0, 0], [0, 0], [0, 0]],
@@ -26,20 +24,6 @@ var player={
     if(this.loaded){
     context.fillText(this.name, this.x-6, this.y-5);
     context.drawImage(playersprite, this.spriteX, this.spriteY, this.width, this.height, this.x, this.y, this.width, this.height);
-    }
-  }
-};
-
-var zombie ={
-  x: Math.round(Math.random()*terrain[0].length),
-  y: 20,
-  loaded: false,
-  health: 10,
-  spriteX: 0,
-  spriteY: 0,
-  update: function(){
-    if(this.loaded){
-      context.drawImage(zombieSprite, this.spriteX, this.spriteY, 32, 64, this.x, this.y, 32, 64);
     }
   }
 };

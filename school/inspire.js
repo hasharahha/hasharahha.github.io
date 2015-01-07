30452706eaba07e0a4c757a1b1f6ea64bd82f479
Jacob Bondley <hasharahha@gmail.com>
2015-01-07 11:34:07 -0500

    Update inspire.js

--- school/inspire.js
+++ school/inspire.js
@@ -2,4 +2,23 @@ var static;
 var notes;
 var save;
 
+if(localStorage.getItem(usernotes)){
 static = document.createElement('textarea');
+static.id ="notes";
+static.width = 1000;
+static.height =500;
+document.body.appendChild(static);
+}
+else{
+  alert("No notes");
+static = document.createElement('textarea');
+static.id ="notes";
+static.width = 1000;
+static.height =500;
+document.body.appendChild(static);
+}
+
+setInterval(function(){
+  notes = document.getElementById("notes").value;
+  localStorage.setItem(usernotes, notes);
+}, 1500);

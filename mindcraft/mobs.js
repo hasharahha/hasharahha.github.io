082235b1063ff6ab031d7d39cffa0834232cc9f3
Jacob Bondley <hasharahha@gmail.com>
2015-01-07 22:42:38 -0500

    Update mobs.js

--- mindcraft/mobs.js
+++ mindcraft/mobs.js
@@ -1,7 +1,7 @@
 var zombieSprite = new Image();
 zombieSprite.src ="https://dl.dropboxusercontent.com/s/3cz7l0dllf7mibd/zombie.png";
 alert("Mob models loaded");
-/*
+
 var zombie ={
   rnd: 0,
   x: Math.random()*1500,
@@ -21,20 +21,5 @@ var zombie ={
     if(this.loaded){
       context.drawImage(zombieSprite, this.spriteX, this.spriteY, this.width, this.height, this.x, this.y, this.width, this.height);
     }
-    if(this.ai){
-      this.rnd = Math.round(Math.random()*5000);
-      if(this.rnd <= 1000){
-        if(terrain[Math.round(this.y/32)][Math.round(this.x/32) +1] ==0){
-          zombie.x += zombie.speed;
-        }
-      }
-    }
-    window.requestAnimationFrame(function(){
-      this.update();
-    }, 1000/30);
   }
 };
-
-
-zombie.update();
-*/

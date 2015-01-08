0d5db704bbe54b06a9122998abfe971033fd7b81
Jacob Bondley <hasharahha@gmail.com>
2015-01-07 22:27:53 -0500

    Update mobs.js

--- mindcraft/mobs.js
+++ mindcraft/mobs.js
@@ -1,2 +1,20 @@
 var zombieSprite = new Image();
 zombieSprite.src ="https://dl.dropboxusercontent.com/s/3cz7l0dllf7mibd/zombie.png";
+
+var zombie ={
+  x: Math.random()*1500,
+  y: 98,
+  height: 64,
+  width: 32,
+  spriteX: 0,
+  spriteY: 0,
+  speed: 1,
+  weight: 2,
+  health: 10,
+  rate: 10/10,
+  update{
+    
+  }
+};
+
+

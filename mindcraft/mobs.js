1ae93773658aec9f8945fff1458efb0cf1262774
Jacob Bondley <hasharahha@gmail.com>
2015-01-07 22:35:33 -0500

    Update mobs.js

--- mindcraft/mobs.js
+++ mindcraft/mobs.js
@@ -1,6 +1,7 @@
 var zombieSprite = new Image();
 zombieSprite.src ="https://dl.dropboxusercontent.com/s/3cz7l0dllf7mibd/zombie.png";
 zombie.update();
+alert("Mob models loaded");
 var zombie ={
   rnd: 0,
   x: Math.random()*1500,

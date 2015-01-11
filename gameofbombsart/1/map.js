481148e34a6602a649b4797acef80b3939813fd5
Jacob Bondley <hasharahha@gmail.com>
2015-01-10 20:36:11 -0500

    Update map.js

--- gameofbombsart/1/map.js
+++ gameofbombsart/1/map.js
@@ -2,4 +2,6 @@ var canvas = document.getElementById("terrain");
 var ctx = canvas.getContext("2d");
 canvas.height =512;
 canvas.width =1024;
+var tiles =new Image();
+tiles.src ="https://dl.dropboxusercontent.com/s/gy986kh0n660ucs/tileset.png";
 alert("all good");

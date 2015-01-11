e622deeb9b668a3b0986fa91c0f5ca92aea6af95
Jacob Bondley <hasharahha@gmail.com>
2015-01-11 15:30:50 -0500

    Update map.js

--- gameofbombsart/1/map.js
+++ gameofbombsart/1/map.js
@@ -5,7 +5,7 @@ canvas.width =1024;
 var tiles =new Image();
 tiles.src ="https://dl.dropboxusercontent.com/s/gy986kh0n660ucs/tileset.png";
 var players =new Image();
-players.src ="https://dl.dropboxusercontent.com/s/ujpdz7erje6hqgi/all_skins%20%281%29.png";
+players.src ="http://bombermine.com/api/ctor/getskin?name=creeper&thumb=1";
 tiles.onload = function(){
  LoadlayerA();
  LoadlayerB();

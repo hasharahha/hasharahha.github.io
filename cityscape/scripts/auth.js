19afff5a50067e9491b45d1fd97fee1d0f03d411
Jacob Bondley <hasharahha@gmail.com>
2015-01-11 18:53:55 -0500

    Update auth.js

--- cityscape/scripts/auth.js
+++ cityscape/scripts/auth.js
@@ -1,1 +1,19 @@
 //auth
+if(localStorage.getItem("user")){
+  alert("You have been successfully logged in")
+  window.location ='http://hasharahha.github.io/cityscape/main.html';
+}
+
+function Auth(){
+  var user = document.getElementById("username").value;
+  var pass = document.getElementById("password").value;
+  
+  if(user =="samuel2013"&&pass=="happynewyear"){
+    localStorage.setItem("user", user);
+    window.location ='http://hasharahha.github.io/cityscape/main.html';
+  }
+  else{
+    localStorage.clear();
+    alert("Incorrect user/pass");
+  }
+}

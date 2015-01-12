dd85551845c3b36947d66c8af30d25900ae59eab
Jacob Bondley <hasharahha@gmail.com>
2015-01-11 19:28:34 -0500

    Update main.js

--- cityscape/scripts/main.js
+++ cityscape/scripts/main.js
@@ -1,1 +1,7 @@
-//
+if(localStorage.getItem("user")){
+  alert("You are allowed");
+}
+else{
+  alert("Sorry you are not authenticated");
+  window.location ='http://hasharahha.github.io/cityscape/';
+}

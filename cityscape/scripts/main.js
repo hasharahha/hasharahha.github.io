068e4f7ee3c7a4f01ab356c23daf866a88326141
Jacob Bondley <hasharahha@gmail.com>
2015-01-12 22:56:16 -0500

    Update main.js

--- cityscape/scripts/main.js
+++ cityscape/scripts/main.js
@@ -116,9 +116,6 @@ function loadLayerB(){
         if(layerB[y][x] ==1){
         ctx.drawImage(tiles, 100, 0, 50, 50, x*50, y*50, 50, 50);
         }
-        if(layerB[y][x] ==3){
-          ctx.drawImage(tiles, 50, 0, 50, 50, x*50, y*50, 50, 50);
-        }
         if(layerB[y][x] ==4){
           ctx.drawImage(tiles, 100, 50, 50, 50, x*50, y*50, 50, 50);
         }

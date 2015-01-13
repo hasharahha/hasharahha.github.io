da12f143ad2167be270de83e7527fae87988dd86
Jacob Bondley <hasharahha@gmail.com>
2015-01-12 19:52:35 -0500

    Update checkauth.js

--- cityscape/scripts/checkauth.js
+++ cityscape/scripts/checkauth.js
@@ -5,6 +5,7 @@ if(localStorage.getItem("user")){
 else{
   document.getElementById("welcome").innerHTML ='Sorry failed to login';
   document.getElementById("msg").innerHTML ='You are not authenticated';
+  setTimeout(function(){
   window.location ='http://hasharahha.github.io/cityscape/';
+}, 2000);
 }
-

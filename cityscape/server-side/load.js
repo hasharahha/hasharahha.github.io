274acd13d329fecadf1f3d8554479f3e0245ed24
Jacob Bondley <hasharahha@gmail.com>
2015-01-14 15:12:40 -0500

    Update load.js

--- cityscape/server-side/load.js
+++ cityscape/server-side/load.js
@@ -1,7 +1,6 @@
 
 try{
 var socket = dojox.socket("http://hasharahha.github.io/cityscape/main.html");
-socket.send(localStorage.getItem("layerB"));
 socket.on("message", function(e){
  static = event.data;
  JSON.parse(static);

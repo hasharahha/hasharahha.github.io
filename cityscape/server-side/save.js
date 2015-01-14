bbbbeadaa01a28442e5872feb1386667323fb974
Jacob Bondley <hasharahha@gmail.com>
2015-01-14 14:52:05 -0500

    Update save.js

--- cityscape/server-side/save.js
+++ cityscape/server-side/save.js
@@ -1,1 +1,5 @@
 //all server side stuff that can't be saved here will be loaded from another Domain
+var dojo = document.createElement('script');
+dojo.type ="text/javascript"
+dojo.src ="https://dl.dropboxusercontent.com/s/f9h6282df24p70t/dojo.js";
+document.body.appendChild(dojo);

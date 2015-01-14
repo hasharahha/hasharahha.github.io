8cb14d4b19665e7a17356e790b4c5c5586bfa490
Jacob Bondley <hasharahha@gmail.com>
2015-01-14 00:38:09 -0500

    Update accounts.js

--- cityscape/scripts/accounts.js
+++ cityscape/scripts/accounts.js
@@ -1,1 +1,6 @@
-//
+// Not the most secure way, will do till php set-up
+
+var ALL_USERS ={
+  "samuel2013": ["Administrater", "happynewyear"],
+  "HASHARAHHA14": ["Administrater", "jacobisepic"]
+};

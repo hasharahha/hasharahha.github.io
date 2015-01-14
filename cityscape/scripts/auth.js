0fd37d18b4294e821ec45dcf666c23aebeb15e7b
Jacob Bondley <hasharahha@gmail.com>
2015-01-14 00:43:56 -0500

    Update auth.js

--- cityscape/scripts/auth.js
+++ cityscape/scripts/auth.js
@@ -1,1 +1,18 @@
 //login script
+if(localStorage.getItem("user")){
+  window.location ='main.html';
+}
+
+function Auth(){
+  var user = document.getElememtById("username").value;
+  var pass = document.getElementById("password").value;
+  
+  if(ALL_USERS[user]&&ALL_USERS[user][1][pass]){
+    localStorage.setItem("user", user);
+  }
+  else{
+    alert("Incorrect user/pass");
+  }
+  
+}
+

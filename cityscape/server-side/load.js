96bdc784d6d4ba0311bd70ced18a4289315a61a0
Jacob Bondley <hasharahha@gmail.com>
2015-01-14 15:20:48 -0500

    Update load.js

--- cityscape/server-side/load.js
+++ cityscape/server-side/load.js
@@ -1,11 +1,12 @@
-
+var errors =" "
 try{
-var socket = dojox.socket("main.html");
+var socket = dojox.socket("/main.html");
 socket.on("message", function(e){
  static = event.data;
  JSON.parse(static);
  data = static;
 });
 }catch(e){
- alert(e);
+ console.log("There was a problem with server: \n"+e);
+ errors +=e;
 }

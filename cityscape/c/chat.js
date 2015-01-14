0ff90a02c29eac42074dd32d4ca2da7441291405
Jacob Bondley <hasharahha@gmail.com>
2015-01-14 12:49:26 -0500

    Create chat.js

---
+++ cityscape/c/chat.js
@@ -0,0 +1,12 @@
+    (function() {
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = '//d3a5udlxrolr3e.cloudfront.net/resource/chat.js';
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
+})();
+
+window.embeddedChatAsyncInit = function() {
+    embeddedChat.init("4326");
+}

e7604f1e69bb8d19110c4fd95b2a65f38ef217f6
Jacob Bondley <hasharahha@gmail.com>
2015-01-14 16:04:27 -0500

    Create main.js

---
+++ cityscape/scripts/main.js
@@ -0,0 +1,169 @@
+var selected =50;
+var world = document.getElementById("world");
+var ctx = world.getContext("2d");
+world.height =750;
+world.width =1400;
+var tiles = new Image();
+tiles.src ="https://dl.dropboxusercontent.com/s/0wfy6onrdqg7oil/tiles.png";
+var plants =new Image();
+plants.src ="https://dl.dropboxusercontent.com/s/tzn1sxolugiiv10/plant_stages.png";
+tiles.onload =function(){
+  loadLayerA();
+  loadLayerB();
+};
+var keys =[];
+
+window.addEventListener("keydown", function(e){
+  keys[e.keyCode] =true;
+  bind();
+}, false);
+window.addEventListener("keyup", function(e){
+  keys[e.keyCode] =false;
+  bind();
+}, false);
+window.addEventListener("click", function(e){
+  var clickX = e.pageX - world.offsetLeft;
+  var clickY = e.pageY - world.offsetTop;
+  var cx = Math.round(clickX/50) -1;
+  var cy = Math.round(clickY/50) -1;
+  layerB[cy][cx] =selected;
+  localStorage.setItem("levelB", JSON.stringify(layerB));
+}, false);
+
+window.addEventListener("mousemove", function(e){
+  var mouseX = e.pageX - world.offsetLeft;
+  var mouseY = e.pageY - world.offsetTop;
+  sx = Math.round(mouseX/50) -1;
+  sy = Math.round(mouseY/50) -1;
+  ctx.rect(sx, sy, 50, 50);
+}, false);
+
+//layers
+var layerA =[
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+var layerB =[
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+if(localStorage.getItem("levelB")){
+  var data = localStorage.getItem("levelB");
+  layerB = JSON.parse(data);
+}
+
+
+function loadLayerA(){
+  ctx.clearRect(-2000, -2000, world.width*2, world.height*2);
+  for(var y=0; y<layerA.length; y++){
+    for(var x=0; x<layerA[y].length; x++){
+      try{
+        if(layerA[y][x] ==0){
+          ctx.drawImage(tiles, 0, 0, 50, 50, x*50, y*50, 50, 50);
+        }
+        if(layerA[y][x] ==1){
+          ctx.drawImage(tiles, 50, 0, 50, 50, x*50, y*50, 50, 50);
+        }
+    
+      }catch(e){
+        
+      }
+    }
+  }
+  window.requestAnimationFrame(function(){
+    loadLayerA();
+  }, 1000/30);
+}
+
+function loadLayerB(){
+  for(var y=0; y<layerB.length; y++){
+    for(var x=0; x<layerB[y].length; x++){
+        if(layerB[y][x] ==1){
+        ctx.drawImage(tiles, 100, 0, 50, 50, x*50, y*50, 50, 50);
+        }
+        if(layerB[y][x] ==4){
+          ctx.drawImage(tiles, 100, 50, 50, 50, x*50, y*50, 50, 50);
+        }
+        if(layerB[y][x] ==5){
+          ctx.drawImage(tiles, 0, 50, 50, 50, x*50, y*50, 50, 50);
+        }
+        if(layerB[y][x] ==6){
+          ctx.drawImage(tiles, 50, 50, 50, 50, x*50, y*50, 50, 50);
+        }
+    }
+  }
+  window.requestAnimationFrame(function(){
+    loadLayerB();
+  }, 1000/30);
+}
+
+
+function bind(){
+  if(keys[38]){
+    
+  }
+  if(keys[40]){
+    
+  }
+  if(keys[37]){
+  
+  }
+  if(keys[39]){
+  
+  }
+  if(keys[70]){
+    document.getElementById("game").webkitRequestFullscreen();
+  }
+}
+
+
+function select(id){
+  selected =id;
+}
+
+/*
+functin msg(message){
+  document.getElementById("message").innerHTML ='alert';
+  document.getElementById("msg").innerHTML =message;
+  document.getElementById("center-popup").style.display ='block';
+
+}
+*/

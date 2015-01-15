9c3b26fb58d6c9b315df42f894ba3bb3df2dad07
98samuelag <98samuelag@gmail.com>
2015-01-14 21:49:42 -0500

    Update action.php

--- cityscape/action.php
+++ cityscape/action.php
@@ -1,1 +1,27 @@
-<!DOCTYPE HTML>
+<?php
+foreach ($_GET as $key => &$val) $val = filter_input(INPUT_GET, $key);
+
+foreach ($_POST as $key => &$val) $val = filter_input(INPUT_POST, $key);
+
+
+?>
+
+<?php
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+
+
+
+$myFile = "comments.txt";
+$fh = fopen($myFile, 'a') or die("can't open file");
+fwrite($fh, "<comment><commentname>");
+fwrite($fh, $name);
+fwrite($fh, "</commentname>");
+fwrite($fh, "$comment");
+fwrite($fh, "</comment>");
+fclose($fh);
+
+
+strip_tags("<?php?>", "<img/>", "<script>")
+
+?>

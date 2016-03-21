1ea832389f9af1c1dc01ca8c46e404b2b24d0285
Jacob Bondley <hasharahha@gmail.com>
2016-03-21 10:56:37 -0400

    Update game.js

--- gobbattle/game.js
+++ gobbattle/game.js
@@ -1,2 +1,17 @@
 var engine = document.getElementById("game").getContext("2d");
-engine.fillRect(0,0);
+var player = {
+ postion:{
+   x:0,
+   y:0
+ },
+ height: 50, width:50, health:100 }
+
+
+function Keys(key){
+  if(keys[])
+}
+
+function gameUpdate(){
+  engine.clear();
+  engine.fillRect(player.position.x, player.position.y, player.height, player.width);
+}

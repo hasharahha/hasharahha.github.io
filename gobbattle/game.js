505c7be032fbad27d92ba5479247112ea54c5c7d
Jacob Bondley <hasharahha@gmail.com>
2016-03-21 11:03:46 -0400

    Update game.js

--- gobbattle/game.js
+++ gobbattle/game.js
@@ -3,7 +3,6 @@ var player = { postion:{ x:0, y:0 }, height: 50, width:50, health:100, speed:10 
 var keys =[];
 window.addEventListener('keydown', function(e){
  keys[e.keyCode]=true;
- alert("Key pressed:"+e.keyCode);
 }, false);
 window.addEventListener('keyup', function(e){
  keys[e.keyCode]=true;

d03638c9a983adab12b8a29cadd73ef9150b7818
Jacob Bondley <hasharahha@gmail.com>
2016-03-22 12:50:18 -0400

    Update game.js

--- gobbattle/game.js
+++ gobbattle/game.js
@@ -1,17 +1,15 @@
-var game = {version:"0.1.1"};
+var game = {version:"0.1.2"};
 var engine = document.getElementById("game").getContext("2d");
-var player = {x:0, y:0, height: 42, width:42, health:100, speed:10, spriteX:0, spriteY:0 };
+var player = {x:0, y:0, height: 42, width:42, health:100, speed:0.8, spriteX:0, spriteY:0 };
 var keys =[];
 var player_sprite = new Image();
 player_sprite.src="player.png";
 player_sprite.onload = function(){ this.loaded=true; };
 window.addEventListener('keydown', function(e){
  keys[e.keyCode]=true;
- Keys();
 }, false);
 window.addEventListener('keyup', function(e){
  keys[e.keyCode]=false;
- Keys();
 }, false);
 
 function Keys(){
@@ -34,12 +32,11 @@ function Keys(){
 function gameUpdate(){
   engine.clearRect(0,0, 1000, 1000);
   engine.drawImage(player_sprite, player.spriteX, player.spriteY, player.width, player.height, player.x, player.y, player.width, player.height);
+  Keys();
 }
 setInterval(function(){
  gameUpdate();
-}, 1);
+}, 10);
 
-setInterval(function(){
- Keys();
-}, 100);
+
 console.log("Current version: "+game.version);

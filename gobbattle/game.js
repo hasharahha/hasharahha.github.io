31fea0e1a96cf670c784075379c2bf3de1f0e329
Jacob Bondley <hasharahha@gmail.com>
2016-03-22 14:47:03 -0400

    Update game.js

--- gobbattle/game.js
+++ gobbattle/game.js
@@ -1,16 +1,10 @@
-var game = {version:"0.2.7", tick:1000/30};
+var game = {version:"1.0.0", tick:50};
 var engine = document.getElementById("game").getContext("2d");
 var player = {x:0, y:0, height: 42, width:42, health:100, speed:5, spriteX:0, spriteY:0 };
 var keys =[];
-var player_sprite = new Image();
-player_sprite.src="player.png";
-player_sprite.onload = function(){ this.loaded=true; };
-window.addEventListener('keydown', function(e){
- keys[e.keyCode]=true;
-}, false);
-window.addEventListener('keyup', function(e){
- keys[e.keyCode]=false;
-}, false);
+var player_sprite = new Image(); player_sprite.src="player.png"; player_sprite.onload = function(){ this.loaded=true; };
+window.addEventListener('keydown', function(e){ keys[e.keyCode]=true; }, false);
+window.addEventListener('keyup', function(e){ keys[e.keyCode]=false; }, false);
 
 function Keys(){ // GOT TO LOVE THEM KEYS ^^ 
  if(keys[38]){ player.y -=player.speed; player.spriteY=0;}
@@ -19,8 +13,6 @@ function Keys(){ // GOT TO LOVE THEM KEYS ^^
  if(keys[39]){ player.x +=player.speed; player.spriteY=42;}
 }
 
-
-
 function renderUpdate(){
   engine.clearRect(0,0, 1000, 1000);
   engine.drawImage(player_sprite, player.spriteX, player.spriteY, player.width, player.height, player.x, player.y, player.width, player.height);
@@ -28,6 +20,6 @@ function renderUpdate(){
 }
 
 renderUpdate();
-setInterval(function(){  Keys(); }, 50);
+setInterval(function(){  Keys(); }, game.tick);
 
 console.log("Current version: "+game.version);

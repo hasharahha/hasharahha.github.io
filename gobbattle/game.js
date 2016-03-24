8bf612be2feab9513042bbcf099ca23a4d8d6852
Jacob Bondley <hasharahha@gmail.com>
2016-03-24 13:46:21 -0400

    Update game.js

--- gobbattle/game.js
+++ gobbattle/game.js
@@ -1,15 +1,17 @@
 var game = {version:"1.0.2", tick:50};
 var engine = document.getElementById("game").getContext("2d");
 var renderMap = document.getElementById("map").getContext("2d");
-var player = {x:0, y:0, height: 42, width:42, health:100, speed:5, spriteX:0, spriteY:0 };
-var keys =[];
+var player = {x:0, y:0, height: 42, width:42, health:100, speed:5, spriteX:0, spriteY:0, collisionX:0, collisionY:0, static:{} };
+var keys =[]; 
 var player_sprite = new Image(); player_sprite.src="player.png"; player_sprite.onload = function(){ this.loaded=true; };
 var tileset = new Image(); tileset.src=map.image; tileset.onload=function(){ this.loaded=true};
 window.addEventListener('keydown', function(e){ keys[e.keyCode]=true; }, false);
 window.addEventListener('keyup', function(e){ keys[e.keyCode]=false; }, false);
 
 function Keys(){ // GOT TO LOVE THEM KEYS ^^ 
- if(keys[38]){ player.y -=player.speed; player.spriteY=0;}
+player.collisionX = Math.round(player.x/32);
+player.collisionY = Math.round(player.y/32);
+ if(keys[38]&&map.data[player.collisionY+1][player.collisionX].walk){ player.y -=player.speed; player.spriteY=0;}
  if(keys[40]){ player.y +=player.speed; player.spriteY=84;}
  if(keys[37]){ player.x -=player.speed; player.spriteY=126;}
  if(keys[39]){ player.x +=player.speed; player.spriteY=42;}

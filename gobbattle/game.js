3173cb8a92ec1a00136b40037e046fb1edf7e292
Jacob Bondley <hasharahha@gmail.com>
2016-03-24 14:23:47 -0400

    Update game.js

--- gobbattle/game.js
+++ gobbattle/game.js
@@ -10,8 +10,10 @@ window.addEventListener('keyup', function(e){ keys[e.keyCode]=false; }, false);
 
 function Keys(){ // GOT TO LOVE THEM KEYS ^^
 
-player.collisionX = Math.floor(player.collisionX/32);
-player.collisionY = Math.floor(player.collisionY/32);
+player.collisionX = Math.round(player.collisionX/32);
+player.collisionY = Math.round(player.collisionY/32);
+
+console.log("Player location: "+player.collisionX+" "+player.collisionY);
  if(keys[38]&&map.tile_id[map.data[player.collisionY-1][player.collisionX]].walk){ player.y -=player.speed; player.spriteY=0;}
  if(keys[40]&&map.tile_id[map.data[player.collisionY+1][player.collisionX]].walk){ player.y +=player.speed; player.spriteY=84;}
  if(keys[37]&&map.tile_id[map.data[player.collisionY][player.collisionX-1]].walk){ player.x -=player.speed; player.spriteY=126;}

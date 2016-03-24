e7f873fee0f52537b64844992a0721ea754a0f04
Jacob Bondley <hasharahha@gmail.com>
2016-03-24 13:40:13 -0400

    Update mapdata.js

--- gobbattle/mapdata.js
+++ gobbattle/mapdata.js
@@ -5,8 +5,8 @@ Just your friendly neighborhood map :D
 var map ={
   image: "tileset.png",
   tile_id:{
-    0:{name:"grass",x:0,y:0},
-    1:{name:"floor",x:192,y:0}
+    0:{name:"grass",walk:true,x:0,y:0},
+    1:{name:"floor",walk:true,x:192,y:0}
   },
   data:[
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],

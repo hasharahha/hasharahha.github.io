3d2dfe5185e17990b8909bc2d8f6a7ef13019b9c
Jacob Bondley <hasharahha@gmail.com>
2016-03-24 14:13:01 -0400

    Update game.js

--- gobbattle/game.js
+++ gobbattle/game.js
@@ -8,9 +8,12 @@ var tileset = new Image(); tileset.src=map.image; tileset.onload=function(){ thi
 window.addEventListener('keydown', function(e){ keys[e.keyCode]=true; }, false);
 window.addEventListener('keyup', function(e){ keys[e.keyCode]=false; }, false);
 
-function Keys(){ // GOT TO LOVE THEM KEYS ^^ 
-player.collisionX = Math.round(player.x/34);
-player.collisionY = Math.round(player.y/34);
+function Keys(){ // GOT TO LOVE THEM KEYS ^^
+
+player.collisionX = player.x+10;
+player.collisionY = player.y+10;
+player.collisionX = Math.round(player.collisionX/32);
+player.collisionY = Math.round(player.collisionY/32);
  if(keys[38]&&map.tile_id[map.data[player.collisionY-1][player.collisionX]].walk){ player.y -=player.speed; player.spriteY=0;}
  if(keys[40]&&map.tile_id[map.data[player.collisionY+1][player.collisionX]].walk){ player.y +=player.speed; player.spriteY=84;}
  if(keys[37]&&map.tile_id[map.data[player.collisionY][player.collisionX-1]].walk){ player.x -=player.speed; player.spriteY=126;}

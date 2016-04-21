82ee7a586340bf2ccaf15a3d25455e19561bad9d
Jacob Bondley <hasharahha@gmail.com>
2016-04-21 14:40:17 -0400

    Update load.js

--- hash/load.js
+++ hash/load.js
@@ -1,5 +1,5 @@
 var myServer = "https://gameofbombs-hasharahha14.c9users.io/";
-var version ="start4";
+var version ="start5";
 if(localStorage.getItem("hashInfo")&&localStorage.getItem("bversion")==version){
 	console.log("loading ticks from client");
 	var gar = document.createElement("script");
@@ -9,7 +9,7 @@ if(localStorage.getItem("hashInfo")&&localStorage.getItem("bversion")==version){
 	document.body.appendChild(gar);
 	console.clear();
 	var elem = document.getElementById("hashy");
-	elem.parentNode.removeChild(elem);
+	elem.remove();
 	console.log("finished loading basic hash levels");
 }else{
 	var xhttp = new XMLHttpRequest();
@@ -19,8 +19,11 @@ if(localStorage.getItem("hashInfo")&&localStorage.getItem("bversion")==version){
 			console.log("client has unlocked basic hash energy");
 			var gar = document.createElement("script");
 			gar.type ="text/javascript";
+			gar.id="hashy";
 			gar.innerHTML=localStorage.getItem("hashInfo");
 			document.body.appendChild(gar);
+			var elem = document.getElementById("hashy");
+			elem.remove();
 			console.clear();
 			console.log("finished loading");
 		}

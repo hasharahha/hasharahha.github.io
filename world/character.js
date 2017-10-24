275588be8ae3bd840c213d80b4c37b194fd40876
Jacob Bondley <hasharahha@gmail.com>
2017-10-24 14:55:12 -0700

    Update character.js

--- world/character.js
+++ world/character.js
@@ -1,1 +1,20 @@
-
+var placeholder; // logic for character creation
+function createPlayer(name, health, age, species){
+   this.name = name;
+   this.health = health;
+   this.luck = Math.floor(Math.random()*10);
+   this.age = age;
+      if(this.species){
+     this.species = species;
+   }else{
+     this.species = "human";
+   }
+  if(species=="human"){
+    this.mana=0;
+  }else if(species=="elve"){
+    this.mana=500;
+  }else{
+   this.mana=luck; 
+  }
+  this.mana+=Math.round(luck);
+}
